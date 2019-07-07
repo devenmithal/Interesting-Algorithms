6afafd32c3c21952c92a44a1bde03ce14428d7e6
Deven Mithal <devenmithal@gmail.com>
2019-07-06 23:13:21 -0400

    Modification in namespace

--- Shunting_Yard_Algorithm/Shunting Yard Algorithm/main.cpp
+++ Shunting_Yard_Algorithm/Shunting Yard Algorithm/main.cpp
@@ -5,23 +5,38 @@
 //  Created by Deven Mithal on 7/1/19.
 //  Copyright © 2019 Deven Mithal. All rights reserved.
 //
-// Shunting-yard algorithm implementation for addition, subtraction, division and multiplication.
+// Shunting-yard algorithm implementation for addition, subtraction,
+// division and multiplication.
 // Exponentiation will be added later, so for the moment all operators under consideration
 // are left-associative.
 
 #include <iostream>
 #include <stack>
 #include <string>
-using namespace std;
+
+int rank(char op){
+    if(op == '*' || op == '/'){
+        return 1;
+    } else if (op == '-' || op == '+'){
+        return 0;
+    } else {
+        std::cerr << "Invalid Operator" << std::endl;
+        return -1;
+    }
+}
+
+int precedence(char firstOp, char secondOp){
+    return rank(firstOp) - rank(secondOp);
+}
 
 void accumulate_number(float& current, char additional){
     current *= 10;
     current += (additional - '0');
 }
 
-float shunting_yard(string expression){
-    stack<char> expression_stack;
-    string digits[] = {"0" , "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+float shunting_yard(std::string expression){
+    std::stack<char> expression_stack;
+    std::string digits[] = {"0" , "1", "2", "3", "4", "5", "6", "7", "8", "9"};
     float current = 0;
     
     for(int i = 0; i < expression.length(); ++i ){
@@ -44,7 +59,7 @@ float shunting_yard(string expression){
 }
 
 void shunting_yard_wrapper(){
-    string expressions[5] = {
+    std::string expressions[5] = {
         "5-4-3+2*7/7",
         "5-(4*2)+3*2/6+(3-2)",
         "2*(9/3*3+1)/2/2",
@@ -59,7 +74,7 @@ void shunting_yard_wrapper(){
     results[3] = ( 17 / ( 15 + 2 ) - 3 ) / 2 * 2;
     results[4] = 2 - 3 - 4 + (69 / 13) + ((6 - 2) - 3) * (6 - (2 - 3)) * (6 - 2 - 3);
     
-    for(string expression : expressions){
+    for(std::string expression : expressions){
         shunting_yard(expression);
     }
 }
